88baf27cc1ee886796fdab86dfd2fe727913db75
Surajit Dasgupta <surajit.dasgupta@servicenow.com>
2025-08-22 17:05:08 +0530

    feat: update installation script

--- setup_grasp_lib.sh
+++ setup_grasp_lib.sh
@@ -5,7 +5,11 @@ set -e
 echo "🚀 Setting up GraSP Library"
 echo "============================="
 echo "Installing GraSP library in development mode..."
+pip install poetry
+poetry install
 cd grasp
 poetry run pip install -e .
 cd ..
+echo "🚀 GraSP Library setup complete"
+echo "============================="
 
