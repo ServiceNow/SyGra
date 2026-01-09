7d537b16e7374600c4a11452d22703761983a917
sidharthenee.nayak <sidharthenee.nayak@servicenow.com>
2026-01-09 14:17:17 +0530

    Removed 500 internal server error code from retry list

--- sygra/utils/constants.py
+++ sygra/utils/constants.py
@@ -59,7 +59,7 @@ MAX_FAILED_ERROR = 10
 MODEL_FAILURE_WINDOW_IN_SEC = 30
 
 # retry the request for below http errors
-RETRYABLE_HTTP_ERROR = [408, 429, 599, 444, 500]
+RETRYABLE_HTTP_ERROR = [408, 429, 599, 444]
 
 # all metadata variables for resumability
 META_TASK_NAME = "task_name"
